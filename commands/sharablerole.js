56e9c14e58c8bcded82f715e6fc9846d171268c5
Jay Williams <course@jaywilliams.me>
2017-12-03 18:00:55 +0000

    Sharablerole command

--- commands/sharablerole.js
+++ commands/sharablerole.js
@@ -23,8 +23,13 @@ class SrCommand extends Command {
     }
 
     async exec(message, args) {
-      if (!message.member.roles.has("378906283727781888")) return;
-     
+      if (!message.member.roles.has("378906283727781888")) return message.channel.send(
+        new util.d.RichEmbed()
+        .setTitle(":octagonal_sign: Stop right there!")
+        .setDescription("You must be `Staff` to use this command")
+        .setFooter(util.makeFooter(message.author), message.author.displayAvatarURL)
+      );
+      
       var possible = await global.mongo.collection("sharableroles").findOne({role: args.sr.id});
       if (possible) return message.reply("Already sharable by <@" + possible.owner + ">")
       await global.mongo.collection("sharableroles").insertOne({
@@ -32,8 +37,7 @@ class SrCommand extends Command {
         owner: args.owner.id
       })
       var fing = await global.mongo.collection("sharableroles").find({}).toArray();
-      
-      message.reply("Added! There are now " + fing.length + " sharable roles.")
+      message.channel.send(new util.d.RichEmbed().setTitle(":white_check_mark: Role " + args.sr.name + " marked as sharable").setColor(0x49bd1a).setFooter(util.makeFooter(message.author), message.author.displayAvatarURL))
     }
 }
 
