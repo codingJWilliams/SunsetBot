1a2fba830662f9ffcb6c562b196d89d31a917d5c
Jay Williams <course@jaywilliams.me>
2017-12-04 16:04:50 +0000

    Fix for clans

--- commands/join.js
+++ commands/join.js
@@ -15,18 +15,19 @@ class JoinCommand extends Command {
     }
 
     async exec(message, args) {
+      var item = await global.mongo.collection("clans").findOne({name: args.clan.toLowerCase()});
+      if (item == null) return message.channel.send(new util.d.RichEmbed().setTitle("Clan not found").setColor(util.red));
       var clans = await global.mongo.collection("clans").find({}).toArray();
       clans.map(c => {
         message.member.removeRole(message.guild.roles.get(c.role))
       })
-      var item = await global.mongo.collection("clans").findOne({name: args.clan.toLowerCase()});
-      if (item == null) return message.channel.send(new util.d.RichEmbed().setTitle("Clan not found").setColor(util.red));
       await message.channel.send(new util.d.RichEmbed()
       .setTitle(":white_check_mark: You joined " + item.displayName)
       .setColor(parseInt(item.color, 16))
       .setFooter(util.makeFooter(message.author), message.author.displayAvatarURL)
       )
       message.member.addRole(message.guild.roles.get(item.role))
+      message.member.removeRole("320199117467025418")
     }
 }
 
