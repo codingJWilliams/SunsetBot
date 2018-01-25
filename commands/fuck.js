b50da8ffa8a8e02f57b6e92a3e51178d9a10fe15
Jay Williams <course@jaywilliams.me>
2018-01-25 19:52:12 +0000

    Add logic

--- commands/fuck.js
+++ commands/fuck.js
@@ -31,42 +31,46 @@ class JoinCommand extends Command {
   }
 
   async exec(message, args) {
-    if (args.op == "sharableroles") {
-      /**@type {Db} */
-      var mongo = global.mongo;
+    /**@type {Db} */
+    var mongo = global.mongo;
+    /**
+     * @type {Message}
+     */
+    var m = await message.channel.send(
+      new RichEmbed()
+      .setColor(0x36393e)
+      .setTitle("Fuck, u sure?")
+      .setDescription("**React <:gasm:310821749467906048> to show that ur sure bro**")
+      .setFooter("Or press :x: to not...")
+    );
+    await m.react(":gasm:310821749467906048");
+    await m.react("❌");
+    var coll = await m.awaitReactions(
       /**
-       * @type {Message}
+       * @param {MessageReaction} reaction
+       * @param {User} user
        */
-      var m = await message.channel.send(
+      (reaction, user) => {
+        return (reaction.emoji.id == "310821749467906048" || reaction.emoji.name == "❌") && user.id == message.author.id;
+      }, {
+        max: 1
+      });
+    if (coll.get("❌")) return message.reply("ok, cancelled. stay safe bro");
+    if (args.op == "sharableroles") {
+      var roles = await mongo.collection("sharableroles").find({}).toArray();
+      roles.map(
+        /**@param {SharableRole} sr */
+        async(sr) => {
+          /** @type {Role} */
+          var discordRole = message.guild.roles.get(sr.role);
+          //await discordRole.delete("Fuck te roles");
+          console.warn("Deleted a role")
+        });
+      message.channel.send(
         new RichEmbed()
         .setColor(0x36393e)
-        .setTitle("Fuck, u sure?")
-        .setDescription("**React <:gasm:310821749467906048> to show that ur sure bro**")
-        .setFooter("Or press :x: to not...")
-      );
-      await m.react(":gasm:310821749467906048");
-      await m.react("❌");
-      var coll = await m.awaitReactions(
-        /**
-         * @param {MessageReaction} reaction
-         * @param {User} user
-         */
-        (reaction, user) => {
-          return (reaction.emoji.id == "310821749467906048" || reaction.emoji.name == "❌") && user.id == message.author.id;
-        }, {
-          max: 1
-        });
-      console.log(coll)
-      if (coll.get("❌")) console.log("xxx")
-      /*var roles = await mongo.collection("sharableroles").find({}).toArray();
-      roles.map(
-        /**@param {SharableRole} sr * /
-        async(sr) => {
-          /** @type {Role} * /
-          var discordRole = message.guild.roles.get(sr.role);
-          await discordRole.delete("Fuck te roles");
-          console.warn("Deleted a role")
-        });*/
+        .setTitle("I think we're done D:")
+      )
     }
   }
 }
